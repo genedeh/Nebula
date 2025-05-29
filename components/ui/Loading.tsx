36d638077663df97a6d5a8dfd09dca62686ee96e
genedeh <genesis.enedeh@gmail.com>
2025-05-29 21:32:50 +0100

    fix: update default color for Loading component to purple

--- components/ui/Loading.tsx
+++ components/ui/Loading.tsx
@@ -1,7 +1,7 @@
 import { View, Text, ActivityIndicator, ColorValue } from 'react-native'
 import React from 'react'
 
-const Loading = ({size="large", color="bg-white"}: {size?: number | "large" | "small" | undefined, color?: ColorValue | undefined}) => {
+const Loading = ({size="large", color="purple"}: {size?: number | "large" | "small" | undefined, color?: ColorValue | undefined}) => {
   return (
       <View className='justify-center items-center'>
           <ActivityIndicator
